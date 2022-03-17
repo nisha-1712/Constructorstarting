7f7f0ba785b8af24ed0b8e2968dd7b7feded4d1b
Nisha Bhavsar <101039209+nisha-1712@users.noreply.github.com>
2022-03-17 14:19:24 +0530

    Constructor declaration

---
+++ src/Nisha/Constructor.java
@@ -0,0 +1,15 @@
+package Nisha;
+
+public class Constructor {
+    int StudentId;
+    String StudentName;
+
+    public Constructor() {
+        System.out.println("This is my First Constructor declration" );
+    }
+
+    public static void main(String[] args) {
+        Constructor obj = new Constructor();
+    }
+}
+
